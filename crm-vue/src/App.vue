36bec75fa1523c09748785338db13d54bc875526
NathenSun <79658980@qq.com>
2017-11-01 00:37:20 +0800

    创建项目，登录页面雏形

---
+++ crm-vue/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+	<div id="app">
+		<el-container style="height:100%">
+			<el-aside style="height:100%">
+				<el-col :span="24">
+				    <el-menu
+				      default-active="2"
+				      class="el-menu-vertical-demo"
+				      background-color="#545c64"
+				      text-color="#fff"
+				      active-text-color="#ffd04b">
+				      <el-menu-item index="1">
+				        <!--<i class="el-icon-menu"></i>-->
+				        <span slot="title">首页</span>
+				      </el-menu-item>
+				      <el-menu-item index="2">
+				        <!--<i class="el-icon-setting"></i>-->
+				        <span slot="title"></span>
+				      </el-menu-item>
+				    </el-menu>
+  				</el-col>
+			</el-aside>
+			<el-container>
+				<el-header>header</el-header>
+				<el-main>
+					<login></login>
+				</el-main>
+			</el-container>
+		</el-container>
+	</div>
+</template>
+
+<script>
+	import login from '@/components/login'
+	import '@/assets/reset.css'
+	export default {
+	  name: 'app',
+	  components: {
+	  	login
+	  }
+	}
+</script>
+
+<style>
+	html,body {
+		height: 100%;
+	}
+	#app {
+		height: 100%;
+	}
+</style>
---
+++ crm-vue/src/components/login.vue
@@ -0,0 +1,73 @@
+<template>
+	<div class="wrap">
+		<div class="login-wrap">
+			<div class="login-img"></div>
+			<h2 class="title">登录</h2>
+			<el-form>
+				<el-form-item>
+					<el-input v-model="login.username"></el-input>
+				</el-form-item>
+				<el-form-item>
+					<el-input v-model="login.password"></el-input>
+				</el-form-item>
+				<!--底下这个要改成routerlink-->
+				<div class="forget-password">忘记密码？</div>
+				<div class="clearfix">
+					<el-checkbox v-model="checked" class="remember-pass">记住密码</el-checkbox>
+					<el-form-item>
+						<el-button type="primary" class="login-btn">登录</el-button>
+					</el-form-item>
+				</div>
+				
+			</el-form>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+	  name: 'login',
+	  data(){
+	  	return {
+	  		login: {
+	  			username: "abc",
+	  			password: "ccc"
+	  		},
+	  		checked: true
+	  	}
+	  }
+	}
+</script>
+
+<style>
+	.wrap {
+		width: 100%;
+		height: 100%;
+	}
+	.login-wrap {
+		width: 400px;
+		height: 400px;
+		/*border: 2px solid #000;*/
+		margin: 0 auto;
+		box-sizing: border-box;
+		padding-top: 30px;
+	}
+	.login-img {
+		width: 50px;
+		height: 50px;
+		border-radius: 50%;
+		margin: 0 auto 40px;
+		background-color: red;
+	}
+	.title {
+		text-align: center;
+		margin-bottom: 40px;
+	}
+	.remember-pass {
+		float: left;
+		
+	}
+	.login-btn {
+		float: right;
+	}
+</style>
