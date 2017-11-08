734133a1dfbf1ada87cdbfbcc0f41725d0475968
NathenSun <79658980@qq.com>
2017-11-08 12:39:31 +0800

    用户管理1

--- crm-vue/src/components/usermanage.vue
+++ crm-vue/src/components/usermanage.vue
@@ -1,10 +1,29 @@
 <template>
-	<div>用户管理</div>
+	<div>
+		<el-col :span="24" class="toolbar" style="padding-bottom: 0px;">
+			<el-form :inline="true">
+				<el-form-item>
+					<el-input placeholder="姓名"></el-input>
+				</el-form-item>
+				<el-form-item>
+					<el-button type="primary">查询</el-button>
+				</el-form-item>
+				<el-form-item>
+					<el-button type="primary">新增</el-button>
+				</el-form-item>
+			</el-form>
+		</el-col>
+	</div>
 </template>
 
 <script>
 	export default {
 	  name: 'usermanage',
+	  data(){
+	  	return {
+	  		
+	  	}
+	  }
 	}
 </script>
 
